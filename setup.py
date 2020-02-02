1c39cd0d2a96add9eb540c320352acb3a65b6b95
rootharold <rootharold@163.com>
2020-02-02 23:32:56 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='LycorisR',
+    version='1.0.0',
+    description="A lightweight recommendation algorithm framework based on LycorisNet.",
+    author="RootHarold",
+    author_email="rootharold@163.com",
+    url="https://github.com/RootHarold/LycorisR",
+    py_modules=['LycorisR'],
+    zip_safe=False,
+    install_requires=['LycorisNet>=2.5', 'numpy>=1.18']
+)
